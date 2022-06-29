5f58fe099dca842d1b303a09779bd0472606edee
takanome_dev <takanomedev221@gmail.com>
2022-06-29 21:13:32 +0000

    refactor: update banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import styles from '../styles/Home.module.css';
 
 import type { NextPage } from 'next';
@@ -5,7 +7,22 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => (
   <div className={styles.container}>
     <main className={styles.main}>
-      <h1 className={styles.title}>Hello World</h1>
+      <div className={styles.banner}>
+        <h1 className={styles.title}>Find your favorite local coffee</h1>
+        <p className={styles.subTitle}>All you need to feel better is coffee</p>
+        <button type="button" className={styles.button}>
+          Discover Coffee Store
+        </button>
+      </div>
+      <div className={styles.hero_img}>
+        <Image
+          src="/static/man_with_coffee.png"
+          alt="Hero Image"
+          width="300px"
+          height="300px"
+          // layout="responsive"
+        />
+      </div>
     </main>
   </div>
 );
